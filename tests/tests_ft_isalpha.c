d52c812bf9b5320768b8f877f8d301143fe799da
Caio De Souza Marinho <caide-so@c2r10p6.42sp.org.br>
2024-10-08 16:55:19 -0300

    add ft_isalpha and tests

--- tests/tests_ft_isalpha.c
+++ tests/tests_ft_isalpha.c
@@ -11,7 +11,7 @@
 /* ************************************************************************** */
 
 #include "minunit.h"
-#include "libft.h"
+#include "../src/libft.h"
 #include "../src/ft_isalpha.c"
 
 MU_TEST(test_isalpha_receiving_A_returns_true)
@@ -27,7 +27,63 @@ MU_TEST(test_isalpha_receiving_A_returns_true)
 	mu_assert_int_eq(expected_result, actual_result);
 }
 
+MU_TEST(test_isalpha_receiving_b_returns_true)
+{
+	// ARRANGE
+	char	input = 'b';
+	int	expected_result = 1; // any number different from 0
+	
+	// ACT
+	int	actual_result = ft_isalpha(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isalpha_receiving_3_returns_false)
+{
+	// ARRANGE
+	char	input = '3';
+	int	expected_result = 0; // any number different from 0
+	
+	// ACT
+	int	actual_result = ft_isalpha(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isalpha_receiving_asterisk_returns_false)
+{
+	// ARRANGE
+	char	input = '*';
+	int	expected_result = 0; // any number different from 0
+	
+	// ACT
+	int	actual_result = ft_isalpha(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
+MU_TEST(test_isalpha_receiving_int_65_returns_true)
+{
+	// ARRANGE
+	int	input = 65;
+	int	expected_result = 1; // any number different from 0
+	
+	// ACT
+	int	actual_result = ft_isalpha(input);
+
+	// ASSERT
+	mu_assert_int_eq(expected_result, actual_result);
+}
+
 MU_TEST_SUITE(ft_isalpha_test_suite)
 {
 	MU_RUN_TEST(test_isalpha_receiving_A_returns_true);
+	MU_RUN_TEST(test_isalpha_receiving_b_returns_true);
+	MU_RUN_TEST(test_isalpha_receiving_3_returns_false);
+	MU_RUN_TEST(test_isalpha_receiving_asterisk_returns_false);
+	MU_RUN_TEST(test_isalpha_receiving_int_65_returns_true);
 }
