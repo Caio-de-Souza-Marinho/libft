772765c8528ef3016328b474844d2b9d2656c419
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-10-16 16:43:31 -0300

    fix: update ft_substr to use malloc instead of calloc

--- tests/tests_ft_substr.c
+++ tests/tests_ft_substr.c
@@ -33,25 +33,9 @@ MU_TEST(test_substr_string_caio_len_8_find_io_return_io)
 	free(actual_result);
 }
 
-MU_TEST(test_substr_string_caio_len_0_should_return_null)
-{
-	// ARRANGE
-	char		s[] = "caio";
-	unsigned int	start = 1;
-	size_t		len = 0;
-
-	// ACT
-	char		*actual_result = ft_substr(s, start, len);
-
-	// ASSERT
-	mu_assert(actual_result == NULL, "the result should be NULL");
-	free(actual_result);
-}
-
 MU_TEST_SUITE(ft_substr_test_suite)
 {
 	MU_RUN_TEST(test_substr_string_caio_find_io_return_io);
 	MU_RUN_TEST(test_substr_string_caio_len_8_find_io_return_io);
-	//MU_RUN_TEST(test_substr_string_caio_len_0_should_return_null);
 }
 
