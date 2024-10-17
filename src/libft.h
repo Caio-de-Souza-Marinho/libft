5a4a59a608e10c18c0d06ce3ce6dc508d2e5b3ff
Caio De Souza Marinho <caide-so@c1r1p1.42sp.org.br>
2024-10-16 22:09:05 -0300

    feature: add ft_split [NOT COMPLETE]

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 19:15:11 by caide-so          #+#    #+#             */
-/*   Updated: 2024/10/16 20:17:53 by caide-so         ###   ########.fr       */
+/*   Updated: 2024/10/16 21:25:19 by caide-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,5 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 #endif
