48a913d45e5906c0a6a1e0275e1484ad464a9ec0
Caio De Souza Marinho <caide-so@c3r4p3.42sp.org.br>
2024-10-18 20:21:33 -0300

    feature: add ft_strmapi and tests

---
+++ src/ft_strmapi.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/18 19:25:38 by caide-so          #+#    #+#             */
+/*   Updated: 2024/10/18 19:47:24 by caide-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int		i;
+	char				*tmp;
+
+	tmp = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	i = 0;
+	while (i < ft_strlen(s))
+	{
+		tmp[i] = (*f)(i, s[i]);
+		i++;
+	}
+	tmp[i] = '\0';
+	return (tmp);
+}
