17f6e7912a502fc78dfbea4f36173fb0a8a37685
Caio De Souza Marinho <caide-so@c1r2p2.42sp.org.br>
2024-10-21 21:44:22 -0300

    feature: add ft_lstadd_back and tests

---
+++ src/ft_lstadd_back.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 21:01:20 by caide-so          #+#    #+#             */
+/*   Updated: 2024/10/21 21:09:30 by caide-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if (!*lst)
+		*lst = new;
+	else
+	{
+		last = ft_lstlast(*lst);
+		last->next = new;
+	}
+}
