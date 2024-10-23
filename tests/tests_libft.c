09878a413bbc515d91cc1a12b14f3184aa5a95c6
Caio De Souza Marinho <caide-so@c3r2p5.42sp.org.br>
2024-10-22 22:27:08 -0300

    feature: add ft_lstiter and tests

--- tests/tests_libft.c
+++ tests/tests_libft.c
@@ -41,6 +41,7 @@
 #include "tests_ft_lstadd_back.c"
 #include "tests_ft_lstdelone.c"
 #include "tests_ft_lstclear.c"
+#include "tests_ft_lstiter.c"
 
 int	main(void)
 {
@@ -83,7 +84,8 @@ int	main(void)
 //	MU_RUN_SUITE(ft_lstlast_test_suite);
 //	MU_RUN_SUITE(ft_lstadd_back_test_suite);
 //	MU_RUN_SUITE(ft_lstdelone_test_suite);
-	MU_RUN_SUITE(ft_lstclear_test_suite);
+//	MU_RUN_SUITE(ft_lstclear_test_suite);
+	MU_RUN_SUITE(ft_lstiter_test_suite);
 
 	MU_REPORT();
 	return MU_EXIT_CODE;
