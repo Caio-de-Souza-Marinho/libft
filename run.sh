56f031923b172bae03549657d9c5b9b85c1fddad
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2024-11-17 15:17:07 -0300

    feature: add get_next_line function and tests
    add directory with files to test gnl function, change main.c file in order to test gnl function, improve Makefile for organization

--- run.sh
+++ run.sh
@@ -1,41 +1,36 @@
 #!/bin/bash
 
 GREEN='\033[1;32m'
-NO_COLOR='\033[0m' # No Color
+NO_COLOR='\033[0m'
 
-# Run norminette and rebuild libft in src
-echo -e "${GREEN}Running Norminette${NO_COLOR}"
+echo -e "\n${GREEN}Running Norminette${NO_COLOR}"
 cd src && \
 norminette && \
-echo # Add newline
 
-echo -e "${GREEN}Rebuilding libft...${NO_COLOR}"
+echo -e "\n${GREEN}Rebuilding libft...${NO_COLOR}"
 make fclean && \
 make && \
-make bonus && \
+# make bonus && \
 
 echo -e "\n${GREEN}Compiling the test program...${NO_COLOR}"
-# Compile the test program
 cd ../tests && \
 make && \
 
-# Run the tests with Valgrind for memory checking
 echo -e "\n${GREEN}Running Valgrind tests...${NO_COLOR}"
 make leakfull && \
 
-echo -e "\n${GREEN}Compiling main.c using the static library...${NO_COLOR}"
-# Compile main.c using the static library
-cd ../ && \
-cc -Wall -Wextra -Werror main.c -L src -lft -o main.out
+# echo -e "\n${GREEN}Compiling main.c using the static library...${NO_COLOR}"
+# cd ../ && \
+# cc -Wall -Wextra -Werror main.c -L src -lft -o main.out
 
 # Check if main compilation was successful
-if [ $? -eq 0 ]; then
-    echo -e "\n${GREEN}Running main function...${NO_COLOR}"
-    ./main.out
-else
-    echo -e "${NO_COLOR}Compilation of main.c failed.${NO_COLOR}"
-    exit 1
-fi
+# if [ $? -eq 0 ]; then
+#    echo -e "\n${GREEN}Running main function...${NO_COLOR}"
+#    ./main.out
+# else
+#    echo -e "${NO_COLOR}Compilation of main.c failed.${NO_COLOR}"
+#    exit 1
+# fi
 
 # Run francinette tests
 # cd src && \
---
+++ src/get_next_line_utils.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caide-so <caide-so@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 18:35:34 by caide-so          #+#    #+#             */
+/*   Updated: 2024/10/28 18:35:42 by caide-so         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	found_newline(t_listg *list)
+{
+	int	i;
+
+	if (list == NULL)
+		return (0);
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i] && i < BUFFER_SIZE)
+		{
+			if (list->str_buf[i] == '\n')
+				return (1);
+			i++;
+		}
+		list = list->next;
+	}
+	return (0);
+}
+
+t_listg	*find_last_node(t_listg *list)
+{
+	if (list == NULL)
+		return (NULL);
+	while (list->next)
+		list = list->next;
+	return (list);
+}
+
+void	copy_str(t_listg *list, char *str)
+{
+	int	i;
+	int	k;
+
+	if (list == NULL)
+		return ;
+	k = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				str[k++] = '\n';
+				str[k] = '\0';
+				return ;
+			}
+			str[k++] = list->str_buf[i++];
+		}
+		list = list->next;
+	}
+	str[k] = '\0';
+}
+
+int	len_until_newline(t_listg *list)
+{
+	int	i;
+	int	len;
+
+	if (list == NULL)
+		return (0);
+	len = 0;
+	while (list)
+	{
+		i = 0;
+		while (list->str_buf[i])
+		{
+			if (list->str_buf[i] == '\n')
+			{
+				len++;
+				return (len);
+			}
+			i++;
+			len++;
+		}
+		list = list->next;
+	}
+	return (len);
+}
+
+void	dealloc(t_listg **list, t_listg *clean_node, char *buf)
+{
+	t_listg	*tmp;
+
+	while (*list)
+	{
+		tmp = (*list)->next;
+		free((*list)->str_buf);
+		free(*list);
+		*list = tmp;
+	}
+	*list = NULL;
+	if (clean_node && clean_node->str_buf[0] != '\0')
+		*list = clean_node;
+	else
+	{
+		free(buf);
+		if (clean_node)
+			free(clean_node);
+	}
+}
