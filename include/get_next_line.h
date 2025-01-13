6764be140bd2d98b9805d4cdfa85c39d3f2a691d
Caio-de-Souza-Marinho <caiosouzamarinho@gmail.com>
2025-01-13 16:28:39 -0300

    refactor: change ft_split so its faster and change gnl header

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 13
+#  define BUFFER_SIZE 100000
 # endif
 
 # include <stdlib.h>
--- src/string/ft_split.c
+++ src/string/ft_split.c
@@ -12,67 +12,57 @@
 
 #include "../../include/libft.h"
 
-static char	*fill_word(char const *s, int start, int end);
-static void	*ft_free(char **strs, int count);
+void	ft_createsplit(char **result, char const *s, char c);
+size_t	ft_addpart(char **result, const char *prev, size_t size, char c);
 
 char	**ft_split(char const *s, char c)
 {
-	char	**res;
+	char	**result;
+
+	if (!s)
+		return (NULL);
+	result = (char **)malloc((ft_word_count(s, c) + 1) * sizeof(char *));
+	if (!result)
+		return (NULL);
+	ft_createsplit(result, s, c);
+	return (result);
+}
+
+void	ft_createsplit(char **result, char const *s, char c)
+{
 	size_t	i;
-	int		j;
-	int		start_word;
+	size_t	j;
+	size_t	size;
+	size_t	prev;
+	size_t	next;
 
 	i = 0;
 	j = 0;
-	start_word = -1;
-	res = ft_calloc((ft_word_count(s, c) + 1), sizeof(char *));
-	if (!res)
-		return (NULL);
-	while (i <= ft_strlen(s))
+	prev = i;
+	next = i;
+	while (1)
 	{
-		if (s[i] != c && start_word < 0)
-			start_word = i;
-		else if ((s[i] == c || i == ft_strlen(s)) && start_word >= 0)
-		{
-			res[j] = fill_word(s, start_word, i);
-			if (!(res[j++]))
-				return (ft_free(res, j - 1));
-			start_word = -1;
-		}
+		if (s[i] == c || s[i] == '\0')
+			next = i;
+		size = next - prev;
+		if (size > 1 || (size == 1 && s[i - 1] != c))
+			j += ft_addpart(&result[j], &s[prev], size, c);
+		if (s[i] == '\0')
+			break ;
+		prev = next;
 		i++;
 	}
-	return (res);
+	result[j] = NULL;
 }
 
-static void	*ft_free(char **strs, int count)
+size_t	ft_addpart(char **result, const char *prev, size_t size, char c)
 {
-	int	i;
-
-	i = 0;
-	while (i < count)
+	if (*prev == c)
 	{
-		free(strs[i]);
-		i++;
+		prev++;
+		size--;
 	}
-	free(strs);
-	return (NULL);
-}
-
-static char	*fill_word(char const *s, int start, int end)
-{
-	char	*word;
-	int		i;
-
-	i = 0;
-	word = malloc((end - start + 1) * sizeof(char));
-	if (!word)
-		return (NULL);
-	while (start < end)
-	{
-		word[i] = s[start];
-		i++;
-		start++;
-	}
-	word[i] = '\0';
-	return (word);
+	*result = (char *)malloc((size + 1) * sizeof(char));
+	ft_strlcpy(*result, prev, size + 1);
+	return (1);
 }
